ab381f4902a24c147671cf2b6210c9f36c96deef
Val Dawson <VDawson647@gmail.com>
2025-04-05 01:06:55 -0400

    popular search and streaming data api pull added

--- src/App.jsx
+++ src/App.jsx
@@ -4,6 +4,7 @@ import Spinner from './components/Spinner';
 import MovieCard from './components/MovieCard';
 import Pagination from './components/Pagination';
 import { useDebounce } from 'react-use';
+import { getTrendingMovies, updateSearchCount } from './appwrite';
 
 
 const API_BASE_URL = "https://api.themoviedb.org/3"
@@ -20,6 +21,7 @@ const App = () => {
   const [searchTerm, setSearchTerm] = useState('');
   const [errorMessage, setErrorMessage] = useState('');
   const [movies, setMovies] = useState([]);
+  const [trendingMovies, setTrendingMovies] = useState([]);
   const [moviePage, setMoviePage] = useState(1);
   const [movieTotalPages, setTotalMoviePages] = useState(500);
   const [isLoading, setIsLoading] = useState(false);
@@ -62,6 +64,11 @@ const App = () => {
       setMovies(data.results||[]);
       // setMoviePage(data.page||[]);
       setTotalMoviePages(data.total_pages||[]);
+
+      // search terms with Appwrite
+      if(query && data.results.length>0){
+        await updateSearchCount(query, data.results[0]);
+      }
       
 
     } catch (error) {
@@ -69,6 +76,15 @@ const App = () => {
       setErrorMessage('Failed to fetch movies. Please try again later.');
     } finally{
       setIsLoading(false);
+    }
+  }
+
+  const loadTrendingMovies = async () => {
+    try {
+      const movies = await getTrendingMovies();
+      setTrendingMovies(movies);
+    } catch (error) {
+      console.error(`Error fetching trending movies: ${error}`);
     }
   }
 
@@ -105,6 +121,10 @@ const App = () => {
     fetchMovies(debouncedSearchTerm, moviePage);
   }, [debouncedSearchTerm, moviePage]);
 
+  useEffect(() => {
+    loadTrendingMovies();
+  }, []);
+
   return (
     <main>
       <div className="pattern"/>
@@ -114,8 +134,23 @@ const App = () => {
           <h1>Find <span className="text-gradient">Movies</span> You'll Enjoy Without The Hassle</h1>
           <Search searchTerm={searchTerm} setSearchTerm={setSearchTerm} />
         </header>
+
+        { trendingMovies.length > 0 && (
+          <section className="trending">
+            <h2>Popular Searches</h2>
+            <ul>
+              {trendingMovies.map((movie, index) => (
+                <li key={movie.$id}> 
+                  <p>{index+1}</p>
+                  <img src={movie.poster_url} alt={movie.title} />
+                </li>
+              ))}
+            </ul>
+          </section>
+        )}
+        
         <section className="all-movies">
-          <h2 className='mt-[40px]'>All Movies</h2>
+          <h2>All Movies</h2>
 
           {isLoading ? (
             <Spinner/>
--- src/components/ModalWin.jsx
+++ src/components/ModalWin.jsx
@@ -1,16 +1,48 @@
 import React, { useRef, useEffect } from 'react';
 
+
 const ModalWin = ({id, title, overview, backdrop_path, openModal, closeModal}) => {
     const ref = useRef();
 
+    const loadMovieStreamingData = () => {
+        const API_KEY = import.meta.env.VITE_TMDB_API_KEY;
+        const options = {
+            method: 'GET',
+            headers: {
+              accept: 'application/json',
+              Authorization: `Bearer ${API_KEY}`,
+            }
+          };
+          
+          fetch(`https://api.themoviedb.org/3/movie/${id}/watch/providers`, options)
+            .then(res => {
+                if(!res.ok) {
+                    throw new Error(`HTTP Error! Status: ${res.status}`);
+                }
+                return res.json();
+            })
+            .then(data => {
+                if(data.results.US){
+                    // console.log('No streaming data available');
+                    console.log('Streaming data:', data.results.US);
+                }else{
+                    console.log('Non Streaming data:', data.results);
+                }
+            })
+            .catch(err => {                
+                console.error('Fetch Error: ',err)
+            });
+    
+    }
+
     useEffect(() => {
         if(openModal){
+            loadMovieStreamingData();
             ref.current.showModal();
         }else{
             ref.current.close();
         }
     }, [openModal])
-
 
   return (
     <dialog ref={ref} id={id} onCancel={closeModal} className="modal">
@@ -18,6 +50,8 @@ const ModalWin = ({id, title, overview, backdrop_path, openModal, closeModal}) =
         <h2>#{id}</h2>
         <img src={backdrop_path ? `https://image.tmdb.org/t/p/w500/${backdrop_path}`:`/no-movie.png`} alt={title}/>
         <p className='modalDescrip'>{overview}</p>
+        <hr />
+
         <button className='modal-close' onClick={closeModal}>Close</button>
     </dialog>
   )
