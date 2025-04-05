af3087318db8d9069a25b8c9a6ef80509dbff129
Val Dawson <VDawson647@gmail.com>
2025-04-05 02:01:10 -0400

    streaming data info added

--- src/components/ModalWin.jsx
+++ src/components/ModalWin.jsx
@@ -1,8 +1,11 @@
-import React, { useRef, useEffect } from 'react';
+import React, { useRef, useState, useEffect } from 'react';
 
 
 const ModalWin = ({id, title, overview, backdrop_path, openModal, closeModal}) => {
     const ref = useRef();
+
+    const [modalMovie, setModalMovie] = useState([]);
+    const [modalBuyList, setModalBuyList] = useState([]);
 
     const loadMovieStreamingData = () => {
         const API_KEY = import.meta.env.VITE_TMDB_API_KEY;
@@ -18,13 +21,18 @@ const ModalWin = ({id, title, overview, backdrop_path, openModal, closeModal}) =
             .then(res => {
                 if(!res.ok) {
                     throw new Error(`HTTP Error! Status: ${res.status}`);
+                    setModalMovie([]);
                 }
                 return res.json();
             })
             .then(data => {
                 if(data.results.US){
                     // console.log('No streaming data available');
-                    console.log('Streaming data:', data.results.US);
+                    // console.log('Streaming data:', data.results.US);
+                    // console.log(data.results.US.buy)
+
+                    setModalMovie(data.results.US||[]);
+                    setModalBuyList(data.results.US.buy||[]);
                 }else{
                     console.log('Non Streaming data:', data.results);
                 }
@@ -46,13 +54,26 @@ const ModalWin = ({id, title, overview, backdrop_path, openModal, closeModal}) =
 
   return (
     <dialog ref={ref} id={id} onCancel={closeModal} className="modal">
+        <button className='modal-close' onClick={closeModal}>Close</button>
         <h1>{title}</h1>
-        <h2>#{id}</h2>
-        <img src={backdrop_path ? `https://image.tmdb.org/t/p/w500/${backdrop_path}`:`/no-movie.png`} alt={title}/>
+        {/* <h2>#{id}</h2> */}
+        <img className='modalMainImg' src={backdrop_path ? `https://image.tmdb.org/t/p/w500/${backdrop_path}`:`/no-movie.png`} alt={title}/>
         <p className='modalDescrip'>{overview}</p>
         <hr />
+        <h3><a href={modalMovie.link} target='_blank'>{title} -- TMDB Page</a></h3>
+        <h4>Available on these platforms:</h4>
+        <ul>
+        {
+            modalBuyList.map((item, index) => (
+                <li key={index} className='modalBuy'>
+                    <img src={`https://image.tmdb.org/t/p/w500/${item.logo_path}`} alt={item.provider_name} width="50" height="50"/>
+                    <p>{item.provider_name}</p>
+                </li>
+            ))
+        }        
+        </ul>
+        
 
-        <button className='modal-close' onClick={closeModal}>Close</button>
     </dialog>
   )
 }
